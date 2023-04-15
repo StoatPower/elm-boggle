c1549432de2c8fa8c5138729ce1714157589c74e
StoatPower <StoatPower@users.noreply.github.com>
2023-04-15 17:01:50 -0400

    fixed words download

--- src/Words.elm
+++ src/Words.elm
@@ -1,16 +1,24 @@
 module Words exposing (..)
 
+import Bytes exposing (Endianness(..))
+import Bytes.Decode as Decode
 import Http
 import RemoteData exposing (WebData)
 
 
 wordsUrl : String
 wordsUrl =
-    "https://api.github.com/repos/raun/Scrabble/contents/words.txt"
+    "https://raw.githubusercontent.com/raun/Scrabble/a9873a163e3ab5d25a209595f738280b144969ce/words.txt"
 
 
 type alias Words =
     List String
+
+
+decodeBytes : Decode.Decoder String
+decodeBytes =
+    Decode.unsignedInt32 BE
+        |> Decode.andThen Decode.string
 
 
 parseWordsText : String -> List String
